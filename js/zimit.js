5d8dd0e6434f89057cbb0bca3515126cb55bb7cd
Almer Almazan <almeralmazan@gmail.com>
2016-11-12 18:40:27 +0800

    Update webpage content
    Revised webpage assets and content

---
+++ js/zimit.js
@@ -0,0 +1,25 @@
+// Copyright 2016 Zimit Media Team
+
+function navbarTransition() {
+	var scroll_top = $(window).scrollTop();
+	var offset_val = 90;
+	if (scroll_top >= offset_val) {
+		$('nav#header-nav img.nav-logo-hide').addClass('nav-logo-show');
+		$('nav#header-nav').addClass('scrolled');
+	} else {
+		$('nav#header-nav img.nav-logo-hide').removeClass('nav-logo-show');
+		$('nav#header-nav').removeClass('scrolled');
+	}
+}
+
+$(document).ready(function(e){
+	// Fixed Navbar Scripts
+	$(window).scroll(navbarTransition);
+
+	// SmoothScrolling to Anchors
+	smoothScroll.init({
+		speed: 1000,
+		easing: 'easeInOutCubic',
+		offset: 79
+	});
+});
